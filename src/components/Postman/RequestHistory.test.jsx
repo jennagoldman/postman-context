8eb6df806dbc979c37b029dcba97ce85bd1d812f
Jenna Goldman <jenna.goldman89@gmail.com>
2020-05-10 22:07:09 -0700

    RequestHistory test passing

--- src/components/Postman/RequestHistory.test.jsx
+++ src/components/Postman/RequestHistory.test.jsx
@@ -2,13 +2,13 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import { RequestProvider } from '../../hooks/RequestProvider.jsx';
-import RequestForm from './RequestForm.jsx';
+import RequestHistory from './RequestHistory.jsx';
 
-describe('RequestForm', () => {
+describe('RequestHistory', () => {
   it('matches a snapshot', () => {
     const wrapper = shallow(
       <RequestProvider>
-        <RequestForm />
+        <RequestHistory />
       </RequestProvider>
     );
     expect(wrapper).toMatchSnapshot();
