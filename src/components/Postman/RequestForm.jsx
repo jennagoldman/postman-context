31e0a1e8c69c02b79f5a10520fefdb794686f601
Jenna Goldman <jenna.goldman89@gmail.com>
2020-05-10 21:49:40 -0700

    RequestForm.css set to use rem and em

--- src/components/Postman/RequestForm.jsx
+++ src/components/Postman/RequestForm.jsx
@@ -6,79 +6,78 @@ const RequestForm = () => {
   const { url, method, body, handleChange, handleRequestSubmit } = useRequestContext();
 
   return (
-  <section className={styles.formContainer}>
-    <form onSubmit={handleRequestSubmit}>
-      <section className={styles.formSection}>
-        <div className={styles.urlDiv}>
+    <section className={styles.formContainer}>
+      <form onSubmit={handleRequestSubmit}>
+        <section className={styles.formSection}>
+          <div className={styles.urlDiv}>
+            <fieldset>
+              <legend>URL</legend>
+              <input 
+                className={styles.urlInput} 
+                type="text" 
+                name="url" 
+                value={url} 
+                id="url" 
+                onChange={handleChange} 
+              />
+            </fieldset>
+          </div>
+          <div className={styles.methodsDiv}>
+            <fieldset>
+              <legend>METHOD</legend>
+              <input 
+                type="radio" 
+                name="method" 
+                value="get" 
+                id="get" 
+                onChange={handleChange} 
+                checked={method === 'get'} 
+              />
+              <label htmlFor="get">Get</label>
+              <input 
+                type="radio" 
+                name="method" 
+                value="put" 
+                id="put" 
+                onChange={handleChange} 
+                checked={method === 'put'}
+              />
+              <label htmlFor="put">Put</label>
+              <input 
+                type="radio" 
+                name="method" 
+                value="post" 
+                id="post" 
+                onChange={handleChange} 
+                checked={method === 'post'} 
+              />
+              <label htmlFor="post">Post</label>
+              <input 
+                type="radio" 
+                name="method" 
+                value="delete" 
+                id="delete" 
+                onChange={handleChange} 
+                checked={method === 'delete'} 
+              />
+              <label htmlFor="delete">Delete</label>
+            </fieldset>
+          </div>
+        </section>
+        <section>
           <fieldset>
-            <legend>URL</legend>
-            <input 
-              className={styles.urlInput} 
-              type="text" 
-              name="url" 
-              value={url} 
-              id="url" 
+            <legend>REQUEST BODY</legend>
+            <textarea 
+              name="body"
+              value={body} 
               onChange={handleChange} 
-            />
+              placeholder="Raw JSON" />
           </fieldset>
-        </div>
-        <div className={styles.methodsDiv}>
-          <fieldset>
-            <legend>METHOD</legend>
-            <input 
-              type="radio" 
-              name="method" 
-              value="get" 
-              id="get" 
-              onChange={handleChange} 
-              checked={method === 'get'} 
-            />
-            <label htmlFor="get">Get</label>
-            <input 
-              type="radio" 
-              name="method" 
-              value="put" 
-              id="put" 
-              onChange={handleChange} 
-              checked={method === 'put'}
-            />
-            <label htmlFor="put">Put</label>
-            <input 
-              type="radio" 
-              name="method" 
-              value="post" 
-              id="post" 
-              onChange={handleChange} 
-              checked={method === 'post'} 
-            />
-            <label htmlFor="post">Post</label>
-            <input 
-              type="radio" 
-              name="method" 
-              value="delete" 
-              id="delete" 
-              onChange={handleChange} 
-              checked={method === 'delete'} 
-            />
-            <label htmlFor="delete">Delete</label>
-          </fieldset>
-        </div>
-      </section>
-      <section>
-        <fieldset>
-          <legend>REQUEST BODY</legend>
-          <textarea 
-            name="body"
-            value={body} 
-            onChange={handleChange} 
-            placeholder="Raw JSON" />
-        </fieldset>
-      </section>
-    
-      <button>Submit</button>
-    </form>
-  </section>
-);
+        </section>
+        <button>Submit</button>
+      </form>
+    </section>
+  );
 };
 
 export default RequestForm;
