8e3785bc2b501a985dfdf3c7d92cdfb19f88a2a2
Jenna Goldman <jenna.goldman89@gmail.com>
2020-05-10 19:25:34 -0700

    copy over RequestHistory component and styles from previous lab

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { RequestProvider } from '../../hooks/RequestProvider.jsx';
 import Postman from '../Postman/Postman.jsx';
+import '../../index.css';
 
 export default function App() {
   return (
---
+++ src/components/Postman/RequestHistory.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import styles from './RequestHistory.css';
+
+const RequestHistory = ({ history, onReset }) => {
+  const pastRequests = history.map((request, i) => (
+    <li key={i}>
+      <h3 className={styles.requestMethod}>{request.method}</h3>
+      <p>{request.url}</p>
+    </li>
+  ));
+
+  return (
+    <section className={styles.historyContainer}>
+      <h2>Request History</h2>
+      <button onClick={onReset}>Clear</button>
+      <ul>
+        {pastRequests}
+      </ul>
+    </section>
+  );
+};
+
+RequestHistory.propTypes = {
+  history: PropTypes.array.isRequired,
+  onReset: PropTypes.func.isRequired
+};
+
+export default RequestHistory;
