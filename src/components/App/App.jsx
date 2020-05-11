45c4e443380327858244a3023e2ae089cfa1fd8c
Jenna Goldman <jenna.goldman89@gmail.com>
2020-05-10 18:44:23 -0700

    update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
+import { RequestProvider } from '../../hooks/RequestProvider.jsx';
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <Header />
+      <RequestProvider>
+        <RequestForm />
+      </RequestProvider>
+    </>
+  )
 }
   
