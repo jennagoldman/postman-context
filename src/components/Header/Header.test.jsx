ec5d762874e6e279fdc2ea407e6c1a1f7262e2a1
Jenna Goldman <jenna.goldman89@gmail.com>
2020-05-10 22:02:51 -0700

    App and Header tests passing

---
+++ src/components/Header/Header.test.jsx
@@ -0,0 +1,11 @@
+  
+import React from 'react';
+import { shallow } from 'enzyme';
+import Header from './Header.jsx';
+
+describe('Header', () => {
+  it('matches a snapshot', () => {
+    const wrapper = shallow(<Header />);
+    expect(wrapper).toMatchSnapshot();
+  });
+});
